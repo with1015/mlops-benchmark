c7ac7659732d0b8a5834999cb2a2ec1a4d1d08e5
Hyunjoon Jeong <with1015@unist.ac.kr>
2023-01-03 17:45:05 +0900

    submit initial code

---
+++ flask/main.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
