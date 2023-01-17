60324c2ff7014eae18a6441fc1412103389e053c
Hyunjoon Jeong <with1015@unist.ac.kr>
2023-01-17 17:06:44 +0900

    [Hotfix] Fix port to 8080

--- flask/app_vision.py
+++ flask/app_vision.py
@@ -62,6 +62,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='51234',
+    app.run(host='127.0.0.1', port='8080',
             use_reloader=False,
             threaded=False)
